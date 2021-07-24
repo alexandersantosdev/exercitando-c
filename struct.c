c3497be7d1d158938fc6232b5392d7c5b5b0a56c
alexandersantosdev <lekxandy@hotmail.com>
2021-07-23 23:29:24 -0300

    Adicionado exercício de struct

--- struct.c
+++ struct.c
@@ -16,33 +16,31 @@ struct Pessoa
     struct Endereco endereco;
 };
 
+int main()
+{
 
-int main(){
-    
     struct Pessoa pessoa;
     printf("Nome: ");
     fgets(pessoa.nome, 40, stdin);
     printf("Rua: ");
-    
-    fgets(pessoa.endereco.rua,40, stdin);
+
+    fgets(pessoa.endereco.rua, 40, stdin);
     printf("Bairro: ");
-    
-    fgets(pessoa.endereco.bairro,40, stdin);
+
+    fgets(pessoa.endereco.bairro, 40, stdin);
     printf("Cidade: ");
-    
-    fgets(pessoa.endereco.cidade,40, stdin);
+
+    fgets(pessoa.endereco.cidade, 40, stdin);
     printf("Numero: ");
-    
+
     scanf("%d", &pessoa.endereco.numero);
 
-    printf("\n\nNome: %s\nRua: %s\nBairro: %s\nCidade: %s\nNumero: %d", 
-        pessoa.nome, 
-        pessoa.endereco.rua, 
-        pessoa.endereco.bairro, 
-        pessoa.endereco.cidade, 
-        pessoa.endereco.numero
-    );
+    printf("\n\nNome: %s\nRua: %s\nBairro: %s\nCidade: %s\nNumero: %d",
+           pessoa.nome,
+           pessoa.endereco.rua,
+           pessoa.endereco.bairro,
+           pessoa.endereco.cidade,
+           pessoa.endereco.numero);
 
     return 0;
-
 }
